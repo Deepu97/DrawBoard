6c183c12a5b88e9c6bc9af7e1f1fe71649d44826
Deepu97 <dy775829@gmail.com>
2026-03-05 18:22:25 +0530

    DrawBoard first commit

---
+++ app.js
@@ -0,0 +1,55 @@
+import dotenv from "dotenv";
+dotenv.config();
+import express from 'express';
+import http from 'http';
+import cors from "cors";
+import handleSocket from './socket/socket.js';
+import { Server } from 'socket.io';
+
+const app = express();
+const server = http.createServer(app);
+const PORT =  4000;
+// Allowed origins
+const allowedOrigins = [
+  "http://127.0.0.1:5501",
+  "http://127.0.0.1:5500",
+  "https://deepu97.github.io"
+];
+
+// ✅ Express APIs ke liye CORS
+// app.use(cors({
+//   origin: function(origin, callback) {
+//     if(!origin) return callback(null, true); // allow non-browser requests
+//     if(allowedOrigins.indexOf(origin) !== -1) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error("Not allowed by CORS"));
+//     }
+//   },
+//   credentials: true
+// }));
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// ✅ Socket.IO ke liye CORS
+const io = new Server(server, {
+  cors: {
+    // origin: function(origin, callback) {
+    //   if(!origin) return callback(null, true);
+    //   if(allowedOrigins.indexOf(origin) !== -1) {
+    //     callback(null, true);
+    //   } else {
+    //     callback(new Error("Not allowed by CORS"));
+    //   }
+    // },
+    origin:"*",
+    methods: ["GET", "POST"],
+    credentials: true
+  }
+});
+
+handleSocket(io);
+
+server.listen(PORT,"0.0.0.0",() => console.log("server is running"));
---
+++ controller/drawing.js
@@ -0,0 +1,28 @@
+
+export const handleJoinRoom = (socket, room) => {
+  socket.join(room);
+ 
+  console.log(`User ${socket.id} joined room ${room}`);
+};
+export const handleMessage=(socket,data)=>{
+ socket.emit("message",data);
+}
+export const handleStart=(socket,data)=>{
+  console.log(data);
+  // socket.broadcast.emit("start",data);
+  socket.to(data.room).emit("start",data);
+}
+export const handleMove=(socket,data)=>{
+  console.log(data,"move");
+  socket.to(data.room).emit("move",data);
+  movePos.push(data);
+  // socket.broadcast.emit("move",data);
+}
+export const handleEnd=(socket,data)=>{
+  // socket.emit("end",data)
+    socket.to(data.room).emit("end",data)
+}
+export const handleAddsticker=(socket,data)=>{
+  // socket.broadcast.emit("addSticker",data);
+   socket.to(data.room).emit("addSticker",data);
+}
